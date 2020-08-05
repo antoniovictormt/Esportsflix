7803b52515326b1a9a5f96f37b3d77c46a2f100d
antoniovictormt <antoniovictormt@hotmail.com>
2020-08-05 16:18:50 -0300

    Update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_DB = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://esportflix.herokuapp.com/';
+  : 'https://esportflix.herokuapp.com';
 
 export default {
   URL_DB,
